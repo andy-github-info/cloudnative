7d35d2a3b52410cfafd13fe688c7794028b94c4a
andy <liyuandong@163.com>
2021-09-27 11:40:26 +0800

    no message

--- httpserver/src/main.go
+++ httpserver/src/main.go
@@ -11,10 +11,32 @@ import (
 
 func main() {
 	http.Handle("/resources", http.NotFoundHandler())
-	http.HandleFunc("/", IndexHandler)
-	http.HandleFunc("/healthz", Healthz)
+	http.Handle("/", SetHeader(http.HandlerFunc(IndexHandler)))
+	http.Handle("/healthz", SetHeader(http.HandlerFunc(HealthzHandler)))
 	log.SetFlags(log.Ldate | log.Ltime)
 	log.Fatal(http.ListenAndServe(":80", logRequest(http.DefaultServeMux)))
+}
+
+func HealthzHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "200\n")
+}
+
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("-----------------")
+	io.WriteString(w, "index\n")
+}
+
+func SetHeader(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			w.Header().Set(k, v[0])
+		}
+		version := os.Getenv("VERSION")
+		if version != "" {
+			w.Header().Set("VERSION", version)
+		}
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func logRequest(handler http.Handler) http.Handler {
@@ -23,25 +45,4 @@ func logRequest(handler http.Handler) http.Handler {
 		log.Printf("%s \n", remoteaddr)
 		handler.ServeHTTP(w, r)
 	})
-}
-
-func Healthz(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "200\n")
-}
-
-func IndexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("-----------------")
-	write := SetHeader(w, req)
-	io.WriteString(write, "index\n")
-}
-
-func SetHeader(w http.ResponseWriter, req *http.Request) http.ResponseWriter {
-	for k, v := range req.Header {
-		w.Header().Set(k, v[0])
-	}
-	version := os.Getenv("VERSION")
-	if version != "" {
-		w.Header().Set("Version", version)
-	}
-	return w
 }
--- httpserver/src/t1.go
+++ httpserver/src/t1.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"io"
+	"log"
+	"net/http"
 )
 
-func main()  {
-	//fmt.Println(os.Getenv("VERSION"))
-	s := "127.0.0.1:30081"
-	l := []string{"no-cache"}
-	fmt.Println(l[0])
-	fmt.Println(strings.Split(s, ":")[0])
+func newPeopleHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "This is the people handler.")
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	// Create sample handler to returns 404
+	mux.Handle("/resources", http.NotFoundHandler())
+
+	// Create sample handler that returns 200
+	mux.Handle("/resources/people/", newPeopleHandler())
+
+	log.Fatal(http.ListenAndServe(":80", mux))
 }
